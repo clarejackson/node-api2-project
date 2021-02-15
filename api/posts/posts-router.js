76388d4a059e72ba707ae0331df9f9a6383e8d67
Clare Jackson <clare-jackson@lambdastudents.com>
2021-02-14 20:44:44 -0500

    MVP done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -8,10 +8,6 @@ router.get("/", (req, res) => {
   res.json({ message: "Hello, World" })
 })
 
-// 1 [GET] /api/posts
-// If there's an error in retrieving the posts from the database:
-// respond with HTTP status code 500.
-// return the following JSON: { message: "The posts information could not be retrieved" }.
 router.get("/api/posts", (req, res) => {
   // console.log(req.query)
 
@@ -27,15 +23,6 @@ router.get("/api/posts", (req, res) => {
   })
 })
 
-// 2 [GET] /api/posts/:id
-// If the post with the specified id is not found:
-
-// return HTTP status code 404 (Not Found).
-// return the following JSON: { message: "The post with the specified ID does not exist" }.
-// If there's an error in retrieving the post from the database:
-
-// respond with HTTP status code 500.
-// return the following JSON: { message: "The post information could not be retrieved" }.
 router.get("/api/posts/:id", (req, res) => {
   posts.findById(req.params.id)
   .then((post) => {
@@ -55,21 +42,6 @@ router.get("/api/posts/:id", (req, res) => {
   })
 })
 
-
-// 3 [POST] /api/posts
-// If the request body is missing the title or contents property:
-
-// respond with HTTP status code 400 (Bad Request).
-// return the following JSON: { message: "Please provide title and contents for the post" }.
-// If the information about the post is valid:
-
-// save the new post the the database.
-// return HTTP status code 201 (Created).
-// return the newly created post.
-// If there's an error while saving the post:
-
-// respond with HTTP status code 500 (Server Error).
-// return the following JSON: { message: "There was an error while saving the post to the database" }.
 router.post("/api/posts", (req, res) => {
   if (!req.body.title || !req.body.contents){
     return res.status(400).json({
@@ -88,25 +60,6 @@ router.post("/api/posts", (req, res) => {
   })
 })
 
-
-// 4 [PUT] /api/posts/:id
-// If the post with the specified id is not found:
-
-// return HTTP status code 404 (Not Found).
-// return the following JSON: { message: "The post with the specified ID does not exist" }.
-// If the request body is missing the title or contents property:
-
-// respond with HTTP status code 400 (Bad Request).
-// return the following JSON: { message: "Please provide title and contents for the post" }.
-// If there's an error when updating the post:
-
-// respond with HTTP status code 500.
-// return the following JSON: { message: "The post information could not be modified" }.
-// If the post is found and the new information is valid:
-
-// update the post document in the database using the new information sent in the request body.
-// return HTTP status code 200 (OK).
-// return the newly updated post.
 router.put("/api/posts/:id", (req, res) => {
   if (!req.body.title || !req.body.contents) {
     return res.status(400).json({
@@ -131,17 +84,6 @@ router.put("/api/posts/:id", (req, res) => {
   })
 })
 
-
-
-// 5 [DELETE] /api/posts/:id
-// If the post with the specified id is not found:
-
-// return HTTP status code 404 (Not Found).
-// return the following JSON: { message: "The post with the specified ID does not exist" }.
-// If there's an error in removing the post from the database:
-
-// respond with HTTP status code 500.
-// return the following JSON: { message: "The post could not be removed" }.
 router.delete("/api/posts/:id", (req, res) => {
   posts.remove(req.params.id)
   .then((post) => {
@@ -161,17 +103,24 @@ router.delete("/api/posts/:id", (req, res) => {
   })
 })
 
-
-// 6 [GET] /api/posts/:id/comments
-// If the post with the specified id is not found:
-
-// return HTTP status code 404 (Not Found).
-// return the following JSON: { message: "The post with the specified ID does not exist" }.
-// If there's an error in retrieving the comments from the database:
-
-// respond with HTTP status code 500.
-// return the following JSON: { message: "The comments information could not be retrieved" }.
-
+router.get("/api/posts/:id/comments", (req, res) => {
+  posts.findPostComments(req.params.id)
+  .then((post) =>{
+    if (post) {
+      res.json(post)
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist"
+      })
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+    res.status(500).json({
+      message: "The comments information could not be retrieved"
+    })
+  })
+})
 
 
 module.exports = router
